db9eabf1ed5dad5bc2c7da2fb6341285075d4f92
cho196 <91933101+cho196@users.noreply.github.com>
2022-04-18 20:26:01 -0700

    added results data

--- .history/timearth/data/res_content_20220418202224.js
+++ .history/timearth/data/res_content_20220418202224.js
@@ -1,47 +1,28 @@
 export const goodResults = [
     {
         title: "You often opted for plant-based meals instead of meat!",
-        desc: " A global shift to a plant-based diet could reduce greenhouse gases caused by food production by 70% within 30 years."
     },
     {
         title: "You frequently used sustainable transportation!",
-        desc:""
     },
     {
         title: "You kept your water usage low through your shower frequency!",
-        desc:""
     },
     {
         title: "You kept your electricity usage to a minimum!",
-        desc:""
     },
     {
         title: "You produced a very small amount of waste!",
-        desc:""
     },
     {
         title: "Your shopping habits were sustainable!",
-        desc:""
     },
 
 ]
 
 export const badResults = [
-    {
-        title:"You have produced a significant amount of waste",
-        desc:"A single litre of used oil can contaminate a million litres of water."
-    },
-    {
-        title:"",
-        desc:""
-        
-    },
-    {
-        title:"",
-        desc:""
-    },
-    {
-        title:"",
-        desc:""
-    },
+    {},
+    {},
+    {},
+    {},
 ]
