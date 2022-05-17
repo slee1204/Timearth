f4e0f574d85b2a50078fa2e0c2f6486e689b024b
seohyun9672 <ajeong4@my.bcit.ca>
2022-05-17 16:53:43 -0700

    modify to be concatenated

--- timearth/comps/Questions/Options.js
+++ timearth/comps/Questions/Options.js
@@ -121,7 +121,7 @@ export default function Options({ q = "question", arr = [], c = "category", acti
                                         var input_sum = Number(e.target.value);
                                         var initialValue = 0;
                                         input_sum = Number(selectedVal.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue));
-                                        console.log(selectedVal, "Sum of the array is", input_sum);
+                                        console.log(selectedVal, "Sum of the array is " + input_sum);
                                         storeChoice(qnum, convertToScore(input_sum));
                                     }
                                 }
