049bc9d0eff0d0a194a219fc9368925037265ffc
seohyun9672 <ajeong4@my.bcit.ca>
2022-05-17 16:54:43 -0700

    add HeaderTxt

--- timearth/data/res_content.js
+++ timearth/data/res_content.js
@@ -1,11 +1,23 @@
+import { getHighest, getLowest } from "./question_content";
+
 export const PageTitle = "Today's Result";
 export const BtnText1 = "Start Over";
 export const BtnText2 = "See Challenges";
+
+
+export const HeaderTxt = {
+    good: {
+        txt: "Great Job!"
+    },
+    bad: {
+        txt: "We Can Try Together!"
+    }
+}
+
 export const goodResults = [
     {
-        header: "Great Job!",
         title: "You frequently used sustainable transportation!",
-        desc:"Using public transportation could save the equivalent of 19 billion litres of gas in a year."
+        desc: "Using public transportation could save the equivalent of 19 billion litres of gas in a year."
     },
     {
         title: "You had plant-based meals for most of this week!",
@@ -13,48 +25,47 @@ export const goodResults = [
     },
     {
         title: "You kept your water usage low through your shower frequency!",
-        desc:"Reducing one minute of shower time could save up to 227 litres of water per month."
+        desc: "Reducing one minute of shower time could save up to 227 litres of water per month."
     },
     {
         title: "You kept your electricity usage to a minimum!",
-        desc:"Just by unplugging unused electronic devices / appliances, you can save up to 10% of energy consumption."
+        desc: "Just by unplugging unused electronic devices / appliances, you can save up to 10% of energy consumption."
     },
     {
         title: "You produced a very small amount of waste!",
-        desc:"Recycling 1 aluminum can could save enough energy to run a TV for 3 hours."
+        desc: "Recycling 1 aluminum can could save enough energy to run a TV for 3 hours."
     },
     {
         title: "Your shopping habits were sustainable!",
-        desc:"If we were to use reusable bags for a lifetime, more than 22,000 plastic bags could be removed from the environment."
+        desc: "If we were to use reusable bags for a lifetime, more than 22,000 plastic bags could be removed from the environment."
     },
 
 ]
 
 export const badResults = [
     {
-        header: "We Can Try Together!",
-        title:"You used personal transportation more often",
-        desc:"One person driving a car could create up to 20lbs of carbon dioxide emissions everyday."
+        title: "You used personal transportation more often",
+        desc: "One person driving a car could create up to 20lbs of carbon dioxide emissions everyday."
     },
     {
-        title:"Try one meal at a time",
-        desc:"If we have one meat-free meal per week, it would bring the same result as taking over 5million cars off the roads annually"
-        
+        title: "Try one meal at a time",
+        desc: "If we have one meat-free meal per week, it would bring the same result as taking over 5million cars off the roads annually"
+
     },
     {
-        title:"You spent longer time for a shower",
-        desc:"With amount of water from 5 more minutes of shower, you can do laundry for 10 more minutes or flush the toilet three times"
+        title: "You spent longer time for a shower",
+        desc: "With amount of water from 5 more minutes of shower, you can do laundry for 10 more minutes or flush the toilet three times"
     },
     {
-        title:"idkidkidk",
-        desc:"Be aware of Phantom Energy in your home while you are unaware of them. They account for about 10 % of household energy consumption"
+        title: "idkidkidk",
+        desc: "Be aware of Phantom Energy in your home while you are unaware of them. They account for about 10 % of household energy consumption"
     },
     {
-        title:"You have produced a significant amount of waste",
-        desc:"A single litre of used oil can contaminate a million litres of water."
+        title: "You have produced a significant amount of waste",
+        desc: "A single litre of used oil can contaminate a million litres of water."
     },
     {
-        title:"You could've used more sustainable shopping material",
-        desc:"Plastic bags are used for 12 min on average, but it takes over 500 years to be degraded in a landfill. Yet, they won/’t break down completely, but become microplastics."
+        title: "You could've used more sustainable shopping material",
+        desc: "Plastic bags are used for 12 min on average, but it takes over 500 years to be degraded in a landfill. Yet, they won/’t break down completely, but become microplastics."
     }
 ]
