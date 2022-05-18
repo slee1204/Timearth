9c7cd290c8acd25912a2796b28fad4d6fb285e37
seohyun9672 <ajeong4@my.bcit.ca>
2022-05-18 15:03:16 -0700

    initial commit

---
+++ timearth/data/global_content.js
@@ -0,0 +1,28 @@
+export const catData = [
+    {
+        title: "Transportation",
+        src: "/transportation.svg"
+
+    },
+    {
+        title: "Food",
+        src: "/food.svg"
+    },
+
+    {
+        title: "Water",
+        src: "/water.svg"
+    },
+    {
+        title: "Electricity",
+        src: "/electricity.svg"
+    },
+    {
+        title: "Waste",
+        src: "/waste.svg"
+    },
+    {
+        title: "Shopping",
+        src: "/shopping.svg"
+    }
+]
